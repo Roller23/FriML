97f5b6debf9eeddb97e972cd91d457cc9f79da2c
Wiktor Kania <roller47@wp.pl>
2020-10-18 17:31:44 +0200

    file parsing

--- main.py
+++ main.py
@@ -1,21 +1,35 @@
 # from tensorflow import keras
 
 import os
-import multiprocessing
-from music21 import converter, instrument
-from multiprocessing import Pool
+import multiprocessing as mp
+import music21 as m21
 
 def convert_midi(path):
-  score = converter.parse(path).chordify()
+  midi = m21.converter.parse(path)
+  parts = m21.instrument.partitionByInstrument(midi)
+  track = None
+  if parts:
+    track = parts.parts[0] if len(parts.parts[0].pitches) > 0 else parts.parts[1]
+  else:
+    track = midi.flat.notes
+  notes = []
+  for event in track:
+    if isinstance(event, m21.note.Note):
+      notes.append(event.pitch)
+    elif isinstance(event, m21.chord.Chord):
+      notes.append('.'.join(str(n) for n in event.pitches))
   print('Converted ' + path)
-  return score
+  return {
+    'notes': notes
+  }
 
-pool = Pool(multiprocessing.cpu_count())
+pool = mp.Pool(mp.cpu_count())
 
 midis_folder = './midis/VGM/'
 midi_files = map(lambda f: midis_folder + f, os.listdir(midis_folder))
 print('Converting midis...')
-scores = pool.map(convert_midi, midi_files)
-print('Done\n', scores)
+songs = pool.map(convert_midi, midi_files)
+print('Done')
 
-# print(score.show('text'))
+for note in songs[1]['notes']:
+  print(note)
