0753221f80c4a307961fc82466c368dffcaa2e88
Wiktor Kania <roller47@wp.pl>
2020-10-18 14:05:55 +0200

    Added midi converting on multiple processes

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 # from tensorflow import keras
 
-from music21 import converter
+import os
+import multiprocessing
+from music21 import converter, instrument
+from multiprocessing import Pool
 
-score = converter.parse('./midis/VGM/zelda.mid').chordify()
+def convert_midi(path):
+  score = converter.parse(path).chordify()
+  print('Converted ' + path)
+  return score
 
-print(score.show('text'))
+pool = Pool(multiprocessing.cpu_count())
+
+midis_folder = './midis/VGM/'
+midi_files = map(lambda f: midis_folder + f, os.listdir(midis_folder))
+print('Converting midis...')
+scores = pool.map(convert_midi, midi_files)
+print('Done\n', scores)
+
+# print(score.show('text'))
