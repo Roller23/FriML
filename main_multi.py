8df0b9e37565d738d1ddf97de839f2eb200aa60e
Szezjo <szezjo@pop-os.localdomain>
2020-11-15 20:02:35 +0100

    added durations, multiple models (TODO: offsets)

---
+++ main_multi.py
@@ -0,0 +1,131 @@
+import os
+import numpy as np
+import music21 as m21
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM, Dense, Dropout, Activation
+from tensorflow.keras.callbacks import ModelCheckpoint
+
+import utils_multi as utils
+
+def train_for_track(notes, durations):
+  pitches = utils.get_unique_pitches(notes)
+  note_to_int = dict((note, number) for number, note in enumerate(pitches))
+
+  unique_durations = utils.get_unique_pitches(durations)
+  duration_to_int = dict((duration, number) for number, duration in enumerate(unique_durations))
+
+  unique_notes_count = len(note_to_int.keys())
+  unique_durations_count = len(duration_to_int.keys())
+
+  sequence_length = 20
+
+  network_input = []
+  network_output = []
+  dur_network_input = []
+  dur_network_output = []
+
+  for song in notes:
+    for i in range(0, len(song) - sequence_length, 1):
+      sequence_in = song[i : i + sequence_length] # s_l notes starting from i offset
+      sequence_out = song[i + sequence_length] # current note + s_l
+      # map strings to numbers
+      network_input.append([note_to_int[char] for char in sequence_in])
+      network_output.append(note_to_int[sequence_out])
+
+  for song in durations:
+    for i in range(0, len(song) - sequence_length, 1):
+      sequence_in = song[i : i + sequence_length] # s_l notes starting from i offset
+      sequence_out = song[i + sequence_length] # current note + s_l
+      # map strings to numbers
+      dur_network_input.append([duration_to_int[char] for char in sequence_in])
+      dur_network_output.append(duration_to_int[sequence_out])
+
+  patterns_count = len(network_input)
+  dur_patterns_count = len(dur_network_input)
+
+  # reshape the input into a format compatible with LSTM layers
+  network_input = np.reshape(network_input, (patterns_count, sequence_length, 1))
+  # normalize input
+  network_input = network_input / float(unique_notes_count) # normalize input
+  network_output = to_categorical(network_output) # convert the vector to a binary matrix
+
+  dur_network_input = np.reshape(dur_network_input, (dur_patterns_count, sequence_length, 1))
+  dur_network_input = dur_network_input / float(unique_durations_count)
+  dur_network_output = to_categorical(dur_network_output)
+
+  print("durpatternscount", dur_patterns_count)
+  print("uniquedurscount", unique_durations_count)
+  model, callbacks = utils.create_model(
+    (network_input.shape[1], network_input.shape[2]),
+    unique_notes_count,
+    loss_dest=0.5
+  )
+
+  print("shape dur", dur_network_input.shape)
+
+  dur_model, dur_callbacks = utils.create_model(
+    (dur_network_input.shape[1], dur_network_input.shape[2]),
+    unique_durations_count,
+    loss_dest=0.5
+  )
+
+  # start training
+  model.fit(network_input, network_output, epochs=10, callbacks=callbacks, batch_size=64)
+  dur_model.fit(dur_network_input, dur_network_output, epochs=10, callbacks=dur_callbacks, batch_size=64)
+
+  return model, dur_model, network_input, dur_network_input
+
+def generate_song(model, network_input, track, dur_model, dur_input, durs, output, length=500):
+  # training finished, generate output song
+  # convert from ints back to class names
+  pitches = utils.get_unique_pitches(track)
+  int_to_note = dict((number, note) for number, note in enumerate(pitches)) # [key => value] = [int => string]
+  unique_durations = utils.get_unique_pitches(durs)
+  int_to_duration = dict((number, duration) for number, duration in enumerate(unique_durations))
+  # print(int_to_note)
+  # print(int_to_duration)
+  prediction_output, dur_prediction_output = utils.construct_song(model, network_input, int_to_note, dur_model, dur_input, int_to_duration, length=length) # predict notes in the new song
+  print('Generated notes\n', prediction_output)
+  print('Generated durations\n', dur_prediction_output)
+  utils.generate_midi(prediction_output, dur_prediction_output, output) # convert output to a .mid file
+
+def main():
+  midis_folder = './midis/n64/'
+  midi_files = map(lambda f: midis_folder + f, os.listdir(midis_folder))
+  #midi_files = list(filter(lambda f: 'ashover_simple_chords' in f, midi_files)) # train only on chord files
+  print('Converting midis...')
+  notes = []
+  durations = []
+  i=0
+  for file in midi_files:
+    if i==5:
+      break
+    try:
+      _notes, _durations = utils.convert_midi(file, target_key='G major')
+    except:
+      os.remove(file)
+    
+    notes.append(_notes)
+    durations.append(_durations)
+    i+=1
+
+  print(durations)
+  model, dur_model, network_input, dur_network_input = train_for_track(notes, durations) # TO DO - add support for durations
+  print('Done')
+  
+  i = 0
+  while True:	
+    try:
+      generate_song(model, network_input, notes, dur_model, dur_network_input, durations, 'output'+str(i)+'.mid')
+    except Exception as e:
+      print(e)
+    i+=1
+    print("Continue?")
+    if input()!='y':
+      break
+
+if __name__ == '__main__':
+ main()
---
+++ utils_multi.py
@@ -0,0 +1,140 @@
+import numpy as np
+import music21 as m21
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM, Dense, Dropout, Activation
+from tensorflow.keras.callbacks import ModelCheckpoint
+from pathlib import Path
+
+def create_model(shape, density, filename="./output/weights.hdf5", loss_dest=0.0001):
+  print('shape', shape)
+  print('density', density)
+  Path(filename).parent.mkdir(parents=True, exist_ok=True) # create needed directories if they don't exist
+  model = Sequential()
+  model.add(LSTM(
+    256,
+    input_shape=shape,
+    return_sequences=True
+  ))
+  model.add(Dropout(0.3))
+  model.add(LSTM(512, return_sequences=True))
+  model.add(Dropout(0.3))
+  model.add(LSTM(256))
+  model.add(Dense(256))
+  model.add(Dropout(0.3))
+  model.add(Dense(density))
+  model.add(Activation('softmax'))
+  model.compile(loss='categorical_crossentropy', optimizer='rmsprop')
+  callbacks_list = [ModelCheckpoint(
+    filename,
+    monitor='loss',
+    save_freq='epoch', # save after every epoch
+    verbose=0,
+    save_best_only=True, # save only if the model is better than in the previous iteration
+    mode='min' # set to min because loss is being monitored
+  )]
+  class haltCallback(tf.keras.callbacks.Callback):
+    def on_epoch_end(self, epoch, logs={}):
+      if (logs.get('loss') <= loss_dest): # stop at certain loss
+        print("\nReached predefined loss, training finished early\n")
+        self.model.stop_training = True
+  trainingStopCallback = haltCallback()
+  callbacks_list.append(trainingStopCallback)
+  return (model, callbacks_list)
+
+def construct_song(model, network_input, int_lut, dur_model, dur_input, int_dur, length=200):
+  print(int_dur)
+  print("Dur_input",dur_input)
+  print("Dur input len", len(dur_input))
+  pattern = network_input[np.random.randint(0, len(network_input) - 1)] # pick a random sequence to start
+  dur_pattern = dur_input[np.random.randint(0, len(dur_input) -1 )]
+  print("Pattern", pattern)
+  print("Dur pattern", dur_pattern)
+  output = []
+  dur_output = []
+  for note_index in range(length):
+    # reshape and normalize
+    prediction_input = np.reshape(pattern, (1, len(pattern), 1)) / float(len(int_lut.keys()))
+    prediction = model.predict(prediction_input, verbose=0)
+    # index = np.argmax(prediction) # generates repetitiveness
+    choice = np.random.choice(prediction[0], 1, p=prediction[0], replace=False)[0]
+    index = np.where(prediction[0] == choice)[0][0] # workaround?
+    result = int_lut[index]
+    output.append(result)
+    # pattern.append(index) # doesnt work
+    pattern = np.append(pattern, index) # workaround
+    pattern = pattern[1 : len(pattern)]
+  #print("Durations:")
+  for dur_index in range(length):
+    # reshape and normalize
+    dur_prediction_input = np.reshape(dur_pattern, (1, len(dur_pattern), 1)) / float(len(int_dur.keys()))
+    dur_prediction = dur_model.predict(dur_prediction_input, verbose=0)
+    # index = np.argmax(prediction) # generates repetitiveness
+    choice = np.random.choice(dur_prediction[0], 1, p=dur_prediction[0], replace=False)[0]
+    index = np.where(dur_prediction[0] == choice)[0][0] # workaround?
+    result = int_dur[index]
+    dur_output.append(result)
+    # pattern.append(index) # doesnt work
+    dur_pattern = np.append(dur_pattern, index) # workaround
+    dur_pattern = dur_pattern[1 : len(dur_pattern)]
+  return output, dur_output
+
+def generate_midi(notes, durs, output='output.mid'):
+  offset = 0
+  output_notes = []
+  output_durations = []
+  for chord, dur in zip(notes, durs):
+    if '.' in chord: # it's a chord
+      notes = []
+      for current_note in chord.split('.'):
+        new_note = m21.note.Note(current_note, duration = m21.duration.Duration(float(dur)))
+        new_note.storedInstrument = m21.instrument.Piano()
+        notes.append(new_note)
+      new_chord = m21.chord.Chord(notes)
+      new_chord.offset = offset
+      output_notes.append(new_chord)
+    else: # it's a single note
+      new_note = m21.note.Note(chord, duration = m21.duration.Duration(float(dur)))
+      new_note.offset = offset
+      new_note.storedInstrument = m21.instrument.Piano()
+      output_notes.append(new_note)
+    offset += 0.5
+
+  midi_stream = m21.stream.Stream(output_notes)
+  midi_stream.write('midi', fp=output)
+
+def convert_midi(path, target_key=None):
+  stream = m21.converter.parse(path)
+  parts = m21.instrument.partitionByInstrument(stream)
+  key = stream.analyze('key')
+  key_str = str(key)
+  print('Key detected ' + key_str)
+  if target_key != None and key_str != target_key:
+    print('Transposing to ' + target_key)
+    interval = m21.interval.Interval(key.tonic, m21.pitch.Pitch('G'))
+    stream = stream.transpose(interval)
+    parts = m21.instrument.partitionByInstrument(stream)
+  track = None
+  if parts:
+    track = parts.parts[0] if len(parts.parts[0].pitches) > 0 else parts.parts[1]
+  else:
+    track = stream.flat.notes
+  notes = []
+  durations = []
+  for event in track:
+    if isinstance(event, m21.note.Note):
+      notes.append(str(event.pitch))
+      durations.append(event.duration.quarterLength)
+    elif isinstance(event, m21.chord.Chord):
+      notes.append('.'.join(str(n) for n in event.pitches))
+      durations.append(event.duration.quarterLength)
+  print('Converted ' + path)
+  return notes, durations
+
+def get_unique_pitches(track):
+  s = set()
+  for song in track:
+    s.update(set(song))
+  return sorted(s)
