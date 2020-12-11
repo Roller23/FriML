5ecf148da859dbea069f4530aa8fb9f264ed8e49
Wiktor Kania <roller47@wp.pl>
2020-12-11 18:06:34 +0100

    changes and bug fixes

--- webapp/webserver.py
+++ webapp/webserver.py
@@ -19,14 +19,17 @@ class HttpHandler(http.server.SimpleHTTPRequestHandler):
       json_string = main_single.generate_for_server('n64', query['key'], query['instrument'])
       os.chdir('./webapp')
       data = json.dumps({'song': json_string})
+      print('sending data')
       self.wfile.write(bytes(data, "utf8"))
       return
     return http.server.SimpleHTTPRequestHandler.do_GET(self)
     
 
 def start_http():
+  port = 8000
   socketserver.TCPServer.allow_reuse_address = True
-  with socketserver.TCPServer(("", 80), HttpHandler) as httpd:
+  with socketserver.TCPServer(("", port), HttpHandler) as httpd:
+    print('http server started on port ' + str(port))
     httpd.serve_forever()
 
 def main():
