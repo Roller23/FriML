d6e5a88b34c98721f2009fe144c4ef5809bac90e
szezjo <szezjo@gmail.com>
2020-12-11 12:18:20 +0100

    Partial transposition (output transp. todo)

--- utils_multi.py
+++ utils_multi.py
@@ -8,6 +8,9 @@ from tensorflow.keras.layers import LSTM, Dense, Dropout, Activation
 from tensorflow.keras.callbacks import ModelCheckpoint
 from pathlib import Path
 
+majors = dict([("A-", 4),("G#", 4),("A", 3),("A#", 2),("B-", 2),("B", 1),("C", 0),("C#", -1),("D-", -1),("D", -2),("D#", -3),("E-", -3),("E", -4),("F", -5),("F#", 6),("G-", 6),("G", 5)])
+minors = dict([("G#", 1), ("A-", 1),("A", 0),("A#", -1),("B-", -1),("B", -2),("C", -3),("C#", -4),("D-", -4),("D", -5),("D#", 6),("E-", 6),("E", 5),("F", 4),("F#", 3),("G-", 3),("G", 2)])
+
 def create_model(shape, density, filename, loss_dest=0.0001):
   print('shape', shape)
   Path(filename).parent.mkdir(parents=True, exist_ok=True) # create needed directories if they don't exist
@@ -119,9 +122,18 @@ def convert_midi(path, target_key=None):
   key_str = str(key)
   print('Key detected ' + key_str)
   if target_key != None and key_str != target_key:
-    print('Transposing to ' + target_key)
-    interval = m21.interval.Interval(key.tonic, m21.pitch.Pitch('G'))
-    stream = stream.transpose(interval)
+    # print('Transposing to ' + target_key)
+    # interval = m21.interval.Interval(key.tonic, m21.pitch.Pitch('G'))
+    # stream = stream.transpose(interval)
+    if key.mode == "major":
+        halfSteps = majors[key.tonic.name]
+    elif key.mode == "minor":
+        halfSteps = minors[key.tonic.name]
+
+    stream = stream.transpose(halfSteps)
+    key = stream.analyze('key')
+    print('Transposed to ' + str(key))
+    print(key.tonic.name + key.mode)
     parts = m21.instrument.partitionByInstrument(stream)
   track = None
   if parts:
