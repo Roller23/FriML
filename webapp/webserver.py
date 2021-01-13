4eeba420a23c8d54d2087a2522a2ba288fff981d
Wiktor Kania <roller47@wp.pl>
2021-01-13 13:01:59 +0100

    separated static web files from ml logic

--- webapp/webserver.py
+++ webapp/webserver.py
@@ -7,22 +7,48 @@ import os
 import sys
 import glob
 
-sys.path.append("../")
+sys.path.append('../')
 import main_single
 
 class HttpHandler(http.server.SimpleHTTPRequestHandler):
+  processed_clients = 0
+  max_clients = 1
+
+  def end_headers (self):
+    self.send_header('Access-Control-Allow-Origin', '*')
+    http.server.SimpleHTTPRequestHandler.end_headers(self)
+  
   def do_GET(self):
-    if self.path.startswith('/data'):
+    if self.path.startswith('/check'):
       self.send_response(200)
-      self.send_header("Content-type", "text/html")
+      self.send_header('Content-type', 'text/html')
       self.end_headers()
-      query = parse_qs(urlparse(self.path).query)
+      available = self.processed_clients < self.max_clients
+      queue = self.max_clients - self.processed_clients + 1
+      data = json.dumps({'clients': self.processed_clients, 'available': available, 'queue': queue})
+      self.wfile.write(bytes(data, 'utf8'))
+      return
+
+    if self.path.startswith('/data'):
+      if self.processed_clients >= self.max_clients:
+        return
+      data = ''
+      self.processed_clients += 1
+      self.send_response(200)
+      self.send_header('Content-type', 'text/html')
+      self.end_headers()
+      q = parse_qs(urlparse(self.path).query)
+      json_string = ''
       os.chdir('..')
-      json_string = main_single.generate_for_server(query['genre'][0], query['key'][0], query['instrument'][0])
+      try:
+        json_string = main_single.generate_for_server(q['genre'][0], q['key'][0], q['instrument'][0])
+      except Exception as err:
+        print(err)
       os.chdir('./webapp')
       data = json.dumps({'song': json_string})
       print('sending data')
-      self.wfile.write(bytes(data, "utf8"))
+      self.wfile.write(bytes(data, 'utf8'))
+      self.processed_clients -= 1
       return
     return http.server.SimpleHTTPRequestHandler.do_GET(self)
     
@@ -30,16 +56,15 @@ class HttpHandler(http.server.SimpleHTTPRequestHandler):
 def start_http():
   port = 8000
   socketserver.TCPServer.allow_reuse_address = True
-  with socketserver.TCPServer(("", port), HttpHandler) as httpd:
+  with socketserver.TCPServer(('', port), HttpHandler) as httpd:
     print('http server started on port ' + str(port))
     httpd.serve_forever()
 
 def main():
   # delete old generated files
-  files = glob.glob('outputs/*')
-  for f in files:
+  for f in glob.glob('outputs/*'):
     os.remove(f)
   start_http()
 
-if __name__ == "__main__":
+if __name__ == '__main__':
   main()
