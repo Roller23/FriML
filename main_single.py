7682516dde6b99c9d07d1c25781b86ff9925f4cb
Wiktor Kania <roller47@wp.pl>
2021-01-17 19:04:43 +0100

    changed song length from 100 to 500

--- main_single.py
+++ main_single.py
@@ -108,7 +108,7 @@ def generate_song(model, network_input, int_to_note, output, length=500):
 
 def generate_for_server(name, key, instrument):
   name = 'rock'
-  length = 100
+  length = 500
   int_to_note, model = load_data(name)
   network_input = []
   for j in range(0, 20):
